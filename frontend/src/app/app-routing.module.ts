c5c9cbdbba49b0b903d9282ad677d17b31c79a71
Fabiola F. Machado <fabiola.machado2@estudante.ifms.edu.br>
2025-09-28 15:06:37 -0400

    mudei varias coisas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,29 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { InfoComponent } from './info/info.component';
-import { QuizComponent } from './quiz/quiz.component';
-import { FilmeserieComponent } from './filmeserie/filmeserie.component';
-import { LegislacaoComponent } from './juridico/legislacao/legislacao.component';
-import { PoliticaprivacidadeComponent } from './juridico/politicaprivacidade/politicaprivacidade.component';
-import { TermoservicoComponent } from './juridico/termoservico/termoservico.component';
-import { TelefonesComponent } from './telefones/telefones.component' ;
 import { NivelfacilComponent } from './nivelfacil/nivelfacil.component' ;
 import { NivelmedioComponent } from './nivelmedio/nivelmedio.component';
+import { NiveldificilComponent } from './niveldificil/niveldificil.component';
 
 
 const routes: Routes = [
   {path: '',     component: HomeComponent},
-  {path:'informacoes', component: InfoComponent},
-  {path:'quiz', component: QuizComponent},
-  {path:'legislacao', component: LegislacaoComponent},
-  {path:'politicadeprivacidade', component: PoliticaprivacidadeComponent},
-  {path:'termodeservico', component: TermoservicoComponent},
-  {path:'filmeseries', component: FilmeserieComponent},
-  {path:'telefones', component: TelefonesComponent},
   {path:'nivelfacil', component: NivelfacilComponent},
-  {path:'nivelmedio',component: NivelmedioComponent}
-
+  {path:'nivelmedio',component: NivelmedioComponent},
+  {path:'niveldificil',component: NiveldificilComponent}
 
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,14 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
-import { InfoComponent } from './info/info.component';
-import { QuizComponent } from './quiz/quiz.component';
 import { HttpClientModule } from '@angular/common/http';
-import { LegislacaoComponent } from './juridico/legislacao/legislacao.component';
-import { TermoservicoComponent } from './juridico/termoservico/termoservico.component';
-import { PoliticaprivacidadeComponent } from './juridico/politicaprivacidade/politicaprivacidade.component';
-import { FilmeserieComponent } from './filmeserie/filmeserie.component';
-import { TelefonesComponent } from './telefones/telefones.component';
 import { NivelfacilComponent } from './nivelfacil/nivelfacil.component';
 import { NivelmedioComponent } from './nivelmedio/nivelmedio.component';
 import { NiveldificilComponent } from './niveldificil/niveldificil.component';
@@ -25,13 +18,6 @@ import { NiveldificilComponent } from './niveldificil/niveldificil.component';
     AppComponent,
     HomeComponent,
     MenuComponent,
-    InfoComponent,
-    QuizComponent,
-    LegislacaoComponent,
-    TermoservicoComponent,
-    PoliticaprivacidadeComponent,
-    FilmeserieComponent,
-    TelefonesComponent,
     NivelfacilComponent,
     NivelmedioComponent,
     NiveldificilComponent
