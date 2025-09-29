578f781208936ab9f30c71882a595a76d6fdbb81
sofia-portilho <sofia.souza2@estudante.ifms.edu.br>
2025-09-29 20:03:13 -0300

    pagina inicial e tudo do home

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Slide {
+  titulo: string;
+  descricao: string;
+  imagem: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  slides: Slide[] = [
+    {
+      titulo: 'Sobre o Projeto',
+      descricao: 'Informações sobre o projeto vão aqui.',
+      imagem: 'assets/img/sobre.jpg'
+    },
+    {
+      titulo: 'Participantes',
+      descricao: 'Lista de participantes do projeto.',
+      imagem: 'assets/img/participantes.jpg'
+    },
+    {
+      titulo: 'Resultados',
+      descricao: 'Resultados ou conquistas do projeto.',
+      imagem: 'assets/img/resultados.jpg'
+    }
+  ];
+
+  currentIndex: number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.startAutoSlide();
+  }
+
+  nextSlide(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+  }
+
+  prevSlide(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+  }
+
+  startAutoSlide(interval: number = 5000): void {
+    setInterval(() => {
+      this.nextSlide();
+    }, interval);
+  }
 }
